5d991a46075a40d6ccf417fcbaa577e50fc68dd7
zhang98888 <386675945@qq.com>
2021-11-28 22:52:32 +0000

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -94,7 +94,8 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  //history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
